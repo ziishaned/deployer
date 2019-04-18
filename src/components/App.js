095e448069f25cdadd831fd25b9ad0a73f873f3d
Zeeshan Ahmad <ziishaned@gmail.com>
2019-04-18 13:15:40 +0400

    Add fontawesome

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
-
 class App extends Component {
   render() {
     return (
-      <div>Test</div>
+      <div>App Page</div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
-    <App />, 
-    document.getElementById('root')
+    <App />,
+    document.getElementById('root'),
 );
